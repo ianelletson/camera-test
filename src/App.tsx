aef6d1feb57995d21eb6bea5bc3b6a4832360406
ian.elletson <dev@ianelletson.com>
2022-04-07 16:40:44 -0500

    Add image capture

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { useRef } from 'react';
-import './App.css';
-import { CameraStream } from './components/CameraStream';
+import { useRef } from "react";
+import "./App.css";
+import { CameraStream } from "./components/CameraStream";
 
 function App() {
-  const videoContainer = useRef<HTMLVideoElement>(null);
+	const videoContainer = useRef<HTMLVideoElement>(null);
+	const framePreviewContainer = useRef<HTMLCanvasElement>(null);
+	const cameraRollContainer = useRef<HTMLDivElement>(null);
 
-  return (
-    <div className="App">
-      <CameraStream videoContainer={videoContainer}/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<CameraStream
+				framePreviewContainer={framePreviewContainer}
+				videoContainer={videoContainer}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/CameraStream.tsx
+++ src/components/CameraStream.tsx
@@ -1,26 +1,108 @@
-import { RefObject, useEffect } from "react";
+import { RefObject, useEffect, useRef, useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
 
-function GetVideo(videoElement: HTMLVideoElement, constraints: { video: boolean }) {
-    navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then((mediaStream) => {
-        const updatedVideoElement = videoElement;
-        updatedVideoElement.srcObject = mediaStream;
-        updatedVideoElement.play();
-    })
-    .catch((err) => {
-        console.dir(err);
-    })
+function GetVideo(video: HTMLVideoElement, constraints: { video: boolean }) {
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then((mediaStream) => {
+			const updatedVideoElement = video;
+			updatedVideoElement.srcObject = mediaStream;
+			updatedVideoElement.play();
+		})
+		.catch((err) => {
+			console.dir(err);
+		});
 }
 
-function CameraStream(props: { videoContainer: RefObject<HTMLVideoElement> }) {
-    useEffect(() => {
-        GetVideo(props.videoContainer.current!, {video: true});
-    });
+function CameraStream(props: {
+	videoContainer: RefObject<HTMLVideoElement>;
+	framePreviewContainer: RefObject<HTMLCanvasElement>;
+}) {
+	const cameraRollContainer = useRef<HTMLDivElement>(null);
+	const [mostRecentImage, setMostRecentImage] = useState<string>("");
 
-    return (
-        <video ref={props.videoContainer} />
-    );
+	useEffect(() => {
+		GetVideo(props.videoContainer.current!, { video: true });
+	});
+
+	return (
+		<>
+			<video
+				onCanPlay={() =>
+					DrawVideoToCanvas(
+						props.videoContainer.current!,
+						props.framePreviewContainer.current!,
+						{ width: 320, height: 240 }
+					)
+				}
+				ref={props.videoContainer}
+			/>
+			<Button
+				onClick={() =>
+					CaptureFrame(
+						props.framePreviewContainer.current!,
+						cameraRollContainer.current!,
+						{ fileType: "image/jpeg", fileName: "download" },
+						setMostRecentImage
+					)
+				}
+			>
+				Capture Still
+			</Button>
+			<canvas ref={props.framePreviewContainer} hidden />
+			<div ref={cameraRollContainer} hidden />
+			<div></div>
+			<Image rounded src={mostRecentImage} />
+			{/* <div>
+				<Card>
+					<Card.Body>
+						<Card.Img src={mostRecentImage} />
+						<Card.Text>Test</Card.Text>
+					</Card.Body>
+				</Card>
+			</div> */}
+		</>
+	);
 }
 
-export { CameraStream }
+function CreateImageFromFrame(props: any) {
+	return <Image src={props.framePreview.toDataURL("image/jpeg")} />;
+}
+
+function CaptureFrame(
+	framePreview: HTMLCanvasElement,
+	cameraRoll: HTMLDivElement,
+	imageProperties: { fileType: string; fileName: string },
+	onFrameCaptured: any
+) {
+	const data = framePreview.toDataURL(imageProperties.fileType);
+
+	const link = document.createElement("a");
+	link.href = data;
+	link.setAttribute("download", imageProperties.fileName);
+	link.innerHTML = `<img src='${data}' alt='thumbnail'/>`;
+
+	cameraRoll.insertBefore(link, cameraRoll.firstChild);
+	onFrameCaptured(data);
+}
+
+function DrawVideoToCanvas(
+	video: HTMLVideoElement,
+	framePreview: HTMLCanvasElement,
+	captureSize: { width: number; height: number }
+) {
+	const context = framePreview.getContext("2d");
+
+	// TODO
+	const w = captureSize.width;
+	const h = captureSize.height;
+	framePreview.width = w;
+	framePreview.height = h;
+
+	return setInterval(() => {
+		context?.drawImage(video, 0, 0, w, h);
+	}, 200);
+}
+
+export { CameraStream };
