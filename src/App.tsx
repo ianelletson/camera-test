fbdf54af22b4e62416009c8f011d09b94087b766
ian.elletson <dev@ianelletson.com>
2022-04-08 10:20:03 -0500

    Add camera selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 import { CameraStream } from "./components/CameraStream";
+
+function CameraSelector(props: { facing: any; onToggle: any }) {
+	const [facingDisplay, setFacingDisplay] = useState("Rear Camera");
+
+	useEffect(() => {
+		if (props.facing === "environment") {
+			setFacingDisplay("Rear Camera");
+		} else {
+			setFacingDisplay("Front Camera");
+		}
+	}, [facingDisplay, props.facing]);
+
+	return (
+		<Button
+			onClick={() => {
+				props.onToggle();
+			}}
+		>
+			{facingDisplay}
+		</Button>
+	);
+}
 
 function App() {
 	const videoContainer = useRef<HTMLVideoElement>(null);
 	const framePreviewContainer = useRef<HTMLCanvasElement>(null);
 	const cameraRollContainer = useRef<HTMLDivElement>(null);
 
+	const [facing, setFacing] = useState("environment");
+
 	const videoConstraints = {
 		audio: false,
 		video: {
-			facingMode: "environment",
+			facingMode: facing,
 			width: 1280,
 			height: 720,
 		},
@@ -18,6 +43,16 @@ function App() {
 
 	return (
 		<div className="App">
+			<CameraSelector
+				facing={facing}
+				onToggle={() => {
+					if (facing === "environment") {
+						setFacing("user");
+					} else {
+						setFacing("environment");
+					}
+				}}
+			/>
 			<CameraStream
 				framePreviewContainer={framePreviewContainer}
 				videoContainer={videoContainer}
