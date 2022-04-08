f4916a18558b3e4e84015259474c720424a84f44
ian.elletson <dev@ianelletson.com>
2022-04-08 10:03:04 -0500

    Add layout container and increase resolution

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,21 @@ function App() {
 	const framePreviewContainer = useRef<HTMLCanvasElement>(null);
 	const cameraRollContainer = useRef<HTMLDivElement>(null);
 
+	const videoConstraints = {
+		audio: false,
+		video: {
+			facingMode: "environment",
+			width: 1280,
+			height: 720,
+		},
+	};
+
 	return (
 		<div className="App">
 			<CameraStream
 				framePreviewContainer={framePreviewContainer}
 				videoContainer={videoContainer}
+				videoConstraints={videoConstraints}
 			/>
 		</div>
 	);
